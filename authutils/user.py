19e86f77586320784ff2000ce5fa06fd9f490c80
Rudyard Richter <rudyardrichter@uchicago.edu>
2018-08-20 16:38:54 -0500

    refactor(no-db): remove user db model

--- authutils/user.py
+++ authutils/user.py
@@ -12,9 +12,9 @@ from authutils.errors import AuthError
 from authutils.token.validate import set_current_token, validate_request
 
 
-def _set_current_user(**kwargs):
+def set_current_user(**kwargs):
     flask.g.user = CurrentUser(**kwargs)
-    set_current_token(flask.g.user.claims)
+    set_current_token(flask.g.user._claims)
     return flask.g.user
 
 
@@ -22,33 +22,33 @@ def _set_current_user(**kwargs):
 #
 # Other modules importing authutils can import ``current_user`` from here,
 # which will use ``_get_or_set_current_user`` to look up the user.
-current_user = LocalProxy(_set_current_user)
+current_user = LocalProxy(set_current_user)
 
 
 class CurrentUser(object):
     """
+    Information about the user which issued a request.
+
     Args:
-        claims (dict): claims from the user's token (if validated already)
-        app_get_session (Function[[flask.Flask], sqlalchemy.orm.Session]):
-            how to get db session context on flask app
+        _claims (dict): claims from the user's token (if validated already)
         jwt_kwargs (dict): keyword arguments to pass to ``validate_request``
 
     Attributes:
-        claims (dict): dictionary of claims from user token
+        _claims (dict): dictionary of claims from user token
         id (str): unique ID for the user
+        username (str): user's username, according to token
         projects (Dict[str, List[str]): mapping of project IDs to roles
+        is_admin (bool): whether the user has admin privileges
     """
 
-    def __init__(self, claims=None, app_get_session=None, jwt_kwargs=None):
-        self.get_session = app_get_session or (lambda app: app.db.session)
+    def __init__(self, claims=None, jwt_kwargs=None):
         jwt_kwargs = jwt_kwargs or {}
         if 'aud' not in jwt_kwargs:
             jwt_kwargs['aud'] = {'openid'}
-        self.claims = claims or validate_request(**jwt_kwargs)
-        self.id = self.claims['sub']
+        self._claims = claims or validate_request(**jwt_kwargs)
+        self.id = self._claims['sub']
         self.username = self._get_user_info('name')
         self.projects = self._get_user_info('projects', default={})
-        self.mapping = {}
 
     def __str__(self):
         str_out = {
@@ -60,31 +60,11 @@ class CurrentUser(object):
 
     def _get_user_info(self, field, default=None):
         return (
-            self.claims
+            self._claims
             .get('context', {})
             .get('user', {})
             .get(field, default)
         )
-
-    @cached_property
-    def user(self):
-        """
-        Return an ``addict.Dict`` (dictionary supporting access by attribute)
-        containing the information from the SQLAlchemy model of this user.
-
-        Requires flask app context.
-
-        We return a ``Dict`` to avoid problems with the user model becoming
-        detached from the original SQLAlchemy session.
-
-        Return:
-            addict.Dict: attributes on the user model
-        """
-        with self.get_session(flask.current_app) as session:
-            user = session.query(User).filter(User.id == self.id).first()
-            if not user:
-                raise ValueError('no user exists with ID: {}'.format(self.id))
-            return Dict(dict(user.__dict__))
 
     @cached_property
     def is_admin(self):
@@ -96,10 +76,7 @@ class CurrentUser(object):
         """
         # Try to just use the user context from the claims. If that doesn't
         # have the ``is_admin`` field then use the database lookup.
-        if self._get_user_info('is_admin') is not None:
-            return self._get_user_info('is_admin')
-        else:
-            return getattr(self.user, 'is_admin')
+        return bool(self._get_user_info('is_admin'))
 
     def require_admin(self):
         """
@@ -110,17 +87,6 @@ class CurrentUser(object):
                 'user ({}) does not have admin privileges'
                 .format(self.id)
             )
-
-    def reload_user(self):
-        """
-        Reset the cached user property and redo the database lookup for this
-        user.
-
-        Return:
-            userdatamodel.models.User
-        """
-        del self.__dict__['user']
-        return self.user
 
     def get_project_ids(self, role='_member_'):
         """
