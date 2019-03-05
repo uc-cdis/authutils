a762b645034c97375a7169a42631286961deb2a4
Fantix King <fantix@uchicago.edu>
2019-03-05 09:26:20 -0600

    feat(notes): auto release notes
    * Also used compatible releases
    * And updated PyPI password

--- setup.py
+++ setup.py
@@ -1,23 +1,41 @@
+from subprocess import check_output
+
 from setuptools import setup, find_packages
+
+
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 setup(
     name="authutils",
-    version="3.0.3",
+    version=get_version(),
     description="Gen3 auth utility functions",
     license="Apache",
     packages=find_packages(),
     install_requires=[
-        "addict>=2.1.1,<3.0.0",
-        "Authlib>=0.4.1,<1.0.0",
+        "addict~=2.1",
+        "Authlib~=0.4",
         "Flask>=0.10.1",
-        "requests>=2.6.0,<3.0.0",
-        "python-keystoneclient==1.8.1",
-        "PyJWT>=1.5.3,<2.0.0",
+        "requests~=2.6",
+        "python-keystoneclient~=1.8",
+        "PyJWT~=1.5",
         "PyYAML>=3.11",
-        "Werkzeug>=0.12.2",
-        "xmltodict>=0.9.2",
-        "cached-property>=1.4.3",
+        "Werkzeug~=0.12",
+        "xmltodict~=0.9",
+        "cached-property~=1.4",
         "cdislogging",
-        "cdiserrors>=0.1.0",
+        "cdiserrors~=0.1",
     ],
 )
