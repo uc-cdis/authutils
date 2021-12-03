6aab537ed950cdfe30d69d22d27a55205d4d4f86
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2021-12-03 13:59:13 -0600

    authlib OAuthClient to OAuth2Client (#60)

--- src/authutils/oauth2/client/client.py
+++ src/authutils/oauth2/client/client.py
@@ -1,4 +1,5 @@
-from authlib.client import OAuthClient
+# rename to maintain backwards compatibility
+from authlib.oauth2.client import OAuth2Client as OAuthClient
 
 
 __all__ = ["OAuthClient"]
---
+++ tests/test_imports.py
@@ -0,0 +1,2 @@
+def test_import_oauth2_client():
+    from authutils.oauth2.client import OAuthClient
